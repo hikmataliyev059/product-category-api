9b34bb85d6a09728735816291c9bb595f5adc386
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-22 17:33:23 +0400

    define category entity configuration with relationships and constraints

---
+++ product-category-api/DAL/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using product_category_api.Models;
+
+namespace product_category_api.DAL.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.HasMany(x => x.Products)
+            .WithOne(x => x.Category)
+            .HasForeignKey(x => x.CategoryId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
