490854d0f0221036a310380801c2d4558a46350c
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-22 17:43:33 +0400

    configure automapper profiles for category and product DTOs

---
+++ product-category-api/Mapper/AutoMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using product_category_api.DTOs.CategoryDtos;
+using product_category_api.DTOs.ProductDtos;
+using product_category_api.Models;
+
+namespace product_category_api.Mapper;
+
+public class AutoMapper : Profile
+{
+    public AutoMapper()
+    {
+        CreateMap<CreateCategoryDto, Category>().ReverseMap();
+        CreateMap<UpdateCategoryDto, Category>().ReverseMap();
+        CreateMap<GetCategoryDto, Category>().ReverseMap();
+        CreateMap<GetAllCategoryDto, Category>().ReverseMap();
+        CreateMap<CreateProductDto, Product>().ReverseMap();
+        CreateMap<UpdateProductDto, Product>().ReverseMap();
+        CreateMap<GetProductDto, Product>().ReverseMap();
+        CreateMap<GetAllProductDto, Product>().ReverseMap();
+    }
+}
