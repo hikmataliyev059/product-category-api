5af2794063fbd9f50e0e3cfdb4f071613234036d
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-22 17:41:34 +0400

    add product DTO for create operation

---
+++ product-category-api/DTOs/ProductDtos/CreateProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace product_category_api.DTOs.ProductDtos;
+
+public class CreateProductDto
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+    public int CategoryId { get; set; }
+}
