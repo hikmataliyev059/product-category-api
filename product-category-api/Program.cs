3460ff67e1db4d14c38bfff4b768f888167a3c19
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-22 17:45:31 +0400

    configure program class with services and middleware

---
+++ product-category-api/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Reflection;
+using FluentValidation.AspNetCore;
+using Microsoft.EntityFrameworkCore;
+using product_category_api.DAL;
+
+namespace product_category_api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers().AddFluentValidation(cfg =>
+            cfg.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContext<AppDbContext>(opt =>
+        {
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DB_URL"));
+        });
+
+        builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
