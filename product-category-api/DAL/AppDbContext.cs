4d909256da3c9bebccd090d34676b7a07162d6cc
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-22 17:34:44 +0400

    configure db context with model configurations

---
+++ product-category-api/DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using product_category_api.Models;
+
+namespace product_category_api.DAL;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
