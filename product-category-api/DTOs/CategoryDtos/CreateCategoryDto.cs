ad16ecdd98de40912651a80ef33cfc8203bdb227
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-22 17:38:43 +0400

    add category DTO and validator for create operation

---
+++ product-category-api/DTOs/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace product_category_api.DTOs.CategoryDtos;
+
+public class CreateCategoryDto
+{
+    public string Name { get; set; }
+}
+
+public class CreateCategoryDtoValidator : AbstractValidator<CreateCategoryDto>
+{
+    public CreateCategoryDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("Name cannot be empty")
+            .MinimumLength(3)
+            .WithMessage("Name must be at least 3 characters long");
+    }
+}
