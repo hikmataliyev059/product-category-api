22c8702449aa348cddc54e255fd41a212765b29e
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-22 17:42:31 +0400

    add DTO for getting all products

---
+++ product-category-api/DTOs/ProductDtos/GetAllProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace product_category_api.DTOs.ProductDtos;
+
+public class GetAllProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+}
