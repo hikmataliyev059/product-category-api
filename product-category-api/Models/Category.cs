b530f4c7e32452d3d898296a2de993e74c9f3328
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-22 16:37:42 +0400

    create category model with relation to products

---
+++ product-category-api/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace product_category_api.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Product>? Products { get; set; }
+}
