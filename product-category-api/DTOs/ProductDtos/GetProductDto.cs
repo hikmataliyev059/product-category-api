f152e8465b411a623d03ad229f1cd448fbd7baf6
hikmataliyev059 <hikmtaliyev444@gmail.com>
2024-12-22 17:42:07 +0400

    add DTO for getting product details

---
+++ product-category-api/DTOs/ProductDtos/GetProductDto.cs
@@ -0,0 +1,11 @@
+﻿namespace product_category_api.DTOs.ProductDtos;
+
+public class GetProductDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } 
+    public double Price { get; set; }
+    public string Description { get; set; }
+    public int? CategoryId { get; set; }
+    public string CategoryName { get; set; } 
+}
